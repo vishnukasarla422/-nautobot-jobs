2d5021ca68f0c3815b3dbb0e1dd0a2d8d81d26da
vishnu-Devops <vishnu-devops@vishnu-Devopss-Mac-mini.local>
2026-02-21 23:15:56 -0600

    Simplify to single __init__.py job

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,4 +1,13 @@
-from nautobot.apps.jobs import register_jobs
-from .add_router_job import AddRouter
+from nautobot.apps.jobs import Job, register_jobs
+
+
+class AddRouter(Job):
+    class Meta:
+        name = "Add Router"
+        description = "Add a new router device to Nautobot"
+
+    def run(self):
+        self.logger.info("Hello from AddRouter!")
+
 
 register_jobs(AddRouter)
